a6bd2dde517f022561fad36868aad6bf803b4ab8
AJYez <amyezikjr@email.neit.edu>
2022-11-14 23:48:25 -0500

    NB

--- Week 6/CanvasExamples/js/animation.js
+++ Week 6/CanvasExamples/js/animation.js
@@ -2,14 +2,14 @@ var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
 var timer = requestAnimationFrame(main);
-var x = 100;
-var y = 300;
+var x = 220;
+var y = 230;
 var speedX = 1;
 var speedY = 1;
 
-var dvd = new Image();
-dvd.src = "images/dvd.png";
-dvd.onload = function(){
+var mm = new Image();
+mm.src = "images/NB.png";
+mm.onload = function(){
     main();
 }
 
@@ -29,13 +29,13 @@ function main(){
     ctx.fill();
     */
     //draw sprite image
-    ctx.drawImage(dvd,x,y,110,80)
+    ctx.drawImage(mm,x,y,90,140)
     x+=speedX;
     y+=speedY;
     if(x>canvas.width-90 || x<0){
         speedX*=-1;
     }
-    if(y>canvas.height-55 || y<0){
+    if(y>canvas.height-140 || y<0){
         speedY*=-1
     }
     console.log(speedX);
